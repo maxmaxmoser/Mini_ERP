dbf35c505c67d68e6d1e4c14308eb02d615b6625
David Marck <david.marck88@gmail.com>
2018-11-06 12:13:56 +0100

    make console wait for input & read cases

---
+++ MiniERP/Cas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniERP
+{
+    class Cas
+    {
+        public int id { get; set; }
+        public string date_depart { get; set; }
+        public int nb_dev { get; set; }
+        public int nb_chef_proj { get; set; }
+        public int efficience { get; set; }
+        public int duree_embauche { get; set; }
+        public int duree_avt_efficacite { get; set; }
+        public string projects { get; set; }
+    }
+
+    class CasList
+    {
+
+        public List<Cas> cas { get; set; }
+
+    }
+}
--- MiniERP/Program.cs
+++ MiniERP/Program.cs
@@ -1,12 +1,27 @@
-﻿namespace MiniERP
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MiniERP
 {
     internal class Program
     {
-        private readonly int efficienceQuestionA = 100;
-        private readonly int efficientQuestionB = 80;
-
         private static void Main(string[] args)
         {
+            Console.WriteLine("========= Jeuxsuilpatron MiniERP project ========");
+            Console.WriteLine("Quel cas voulez-vous traiter?");
+            Console.WriteLine("\t1 - Cas 1 : 100% efficience, 3 projets");
+            Console.WriteLine("\t2 - Cas 2 : 80% efficience, 3 projets");
+            Console.WriteLine("\t3 - Cas 3 : 120% efficience, 4 projets");
+
+            Console.Write("-->");
+            int response = int.Parse(Console.ReadLine());
+            using (StreamReader r = new StreamReader("..\\..\\config\\cas.json"))
+            {
+                string json = r.ReadToEnd();
+                CasList items = JsonConvert.DeserializeObject<CasList>(json);
+            }
         }
     }
 }
